d77c5260a7321c85ae509bfbe9529b6a362a3352
Leevi Aattola <leevi.aattola@geniem.com>
2024-08-09 00:19:21 +0300

    feat: better err handling still doesn't leave the channel when stream is done? ffmpeg does not end progress? todo!

--- main.go
+++ main.go
@@ -12,9 +12,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"os/signal"
 	"strconv"
-	"syscall"
 )
 
 const (
@@ -28,7 +26,10 @@ func ready(s *discordgo.Session, event *discordgo.Ready) {
 
 	fmt.Println("Ready")
 	// Set the playing status.
-	s.UpdateCustomStatus("gaming")
+	err := s.UpdateCustomStatus("gaming")
+	if err != nil {
+		fmt.Println("Error attempting to set status to gaming")
+	}
 }
 
 func main() {
@@ -48,44 +49,52 @@ func main() {
 
 	dg.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates
 
-	// Open the websocket and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("Error opening Discord session: ", err)
+		log.Panic("Error opening Discord session: ", err)
 	}
 
+	//hardcoded channel ids change as needed
 	vc, err := dg.ChannelVoiceJoin("214761475422683136", "214761475422683137", false, true)
 	if err != nil {
-		fmt.Errorf("error joining voice channel: %v", err)
-		os.Exit(1)
+		log.Panic("Error joining voice channel: ", err)
 	}
 
-	vc.Speaking(true)
+	err = vc.Speaking(true)
+	if err != nil {
+		log.Panic("Error setting speaking: ", err)
+	}
 
 	r, w, err := os.Pipe()
 	if err != nil {
-		fmt.Errorf("error creating pipe: %v", err)
-		return
+		log.Panic("Error creating pipe: ", err)
 	}
 
-	ytdlCmd := exec.Command("yt-dlp", "-x", "--audio-format", "opus", "--audio-quality", "0", "-o", "-", "https://www.youtube.com/watch?v=I4hrPA2a9RI")
+	// hard coded youtube video id change as needed
+	ytdlCmd := exec.Command("yt-dlp", "-x", "--audio-format", "opus", "--audio-quality", "0", "-o", "-", "https://www.youtube.com/watch?v=kpwNjdEPz7E")
 	ytdlCmd.Stdout = w
 	err = ytdlCmd.Start()
 	if err != nil {
-		fmt.Errorf("error starting yt-dlp: %v", err)
-		return
+		log.Panic("Error starting yt-dlp: ", err)
 	}
-	defer ytdlCmd.Wait()
 
-	ffmpegCmd := exec.Command("ffmpeg", "-i", "pipe:0", "-f", "s16le", "-ar", strconv.Itoa(FRAME_RATE), "-ac", strconv.Itoa(CHANNELS), "pipe:1")
+	ffmpegCmd := exec.Command("ffmpeg", "-i", "pipe:0", "-f", "s16le", "-ar", strconv.Itoa(FRAME_RATE), "-ac", strconv.Itoa(CHANNELS), "-")
 	//ffmpegCmd := exec.Command("ffmpeg", "-i", "pipe:0", "-ar", "48000", "-ac", "2", "-b:a", "64k", "-f", "ogg", "-c:a", "libopus", "-application", "audio", "-frame_size", "960", "-")
 	ffmpegCmd.Stdin = r
 
 	//cmd := exec.Command("yt-dlp", "-x", "--audio-format", "opus", "--audio-quality", "0", "-o", "-", "https://www.youtube.com/watch?v=I4hrPA2a9RI")
 	stdout, _ := ffmpegCmd.StdoutPipe()
-	ffmpegCmd.Start()
+	err = ffmpegCmd.Start()
+	if err != nil {
+		log.Panic("Error starting ffmpeg: ", err)
+	}
 
-	defer vc.Disconnect()
+	defer func(vc *discordgo.VoiceConnection) {
+		err := vc.Disconnect()
+		if err != nil {
+			fmt.Println("Error disconnecting from voice channel: ", err)
+		}
+	}(vc)
 
 	encoder, err := gopus.NewEncoder(FRAME_RATE, CHANNELS, gopus.Audio)
 
@@ -95,12 +104,12 @@ func main() {
 		audioBuffer := make([]int16, FRAME_SIZE*CHANNELS)
 		err = binary.Read(buffer, binary.LittleEndian, &audioBuffer)
 		if err == io.EOF || errors.Is(err, io.ErrUnexpectedEOF) {
-			fmt.Errorf("error reading from ffmpeg stdout: %v", err)
-			return
+			log.Printf("EOF: error reading from ffmpeg stdout: %v", err)
+			break
 		}
 		if err != nil {
-			fmt.Errorf("error reading from ffmpeg stdout: %v", err)
-			return
+			log.Printf("error reading from ffmpeg stdout: %v", err)
+			break
 		}
 
 		opus, err := encoder.Encode(audioBuffer, FRAME_SIZE, MAX_BYTES)
@@ -115,21 +124,4 @@ func main() {
 
 		vc.OpusSend <- opus
 	}
-
-	//	defer vc.Disconnect()
-	//	scanner := bufio.NewScanner(stdout)
-	//	for scanner.Scan() {
-	//		m := scanner.Bytes()
-	//
-	//		vc.OpusSend <- m
-	//	}
-	ffmpegCmd.Wait()
-
-	fmt.Println("Pyörii.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
 }
